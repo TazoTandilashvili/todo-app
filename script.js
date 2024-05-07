2c9169d99678c636dcef849af6b82411bc69c1a9
tazo tandilashvii <tazo.tandilashvili2004@gmail.com>
2024-05-07 19:08:10 +0400

    push newjs

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ const checkBox = document.getElementById('checkBox');
 let text = ''
 checkBox.addEventListener('click', function(){
   if (checkBox.checked){
+    const task = document.querySelector('.taskProperty');
     text = textInput.value;
+    task.textContent = text;
+    console.log(task.textContent);
     console.log(text)
   } 
   
